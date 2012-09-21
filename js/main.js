ac6f2bda2cb178b7e08d71695bc0d71a78aa8a42
mrjoseph <trev_jos@hotmail.com>
2012-09-21 13:27:02 +0100

    Creating the UI

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+$(document).ready(function(){
+    $('#box').fadeIn();
+    centerElm('#box');
+    $(window).resize(function(){
+        centerElm('#box');
+    });
+    drawCanvas();
+});
+
+function centerElm(elem){
+    //Get the height of the window
+    var window_height = $(window).height();
+    // Get the center of the window
+    var window_center = window_height/2;
+    //get the height of the container
+    var elem_height = $(elem).height();
+    //get the middle of the element
+    var elem_middle = elem_height/2;
+    // find the middle
+    var center_pos = window_center - elem_middle;
+    //position element in the center
+    if(window_height>elem_height){
+        $(elem).css("marginTop", center_pos);
+    } else {
+        $(elem).css("marginTop", '0');
+    }
+}
+
 
